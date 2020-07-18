1eb79d2b881563f74d4f36a908e415ebaec58b37
Spenser <spenser.davies@gmail.com>
2020-07-18 09:37:02 -0600

    init

---
+++ wwwsrc/src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dashboard container-fluid text-success">
+    <div class="row mt-4">
+      <div class="col-4 pb-3 border-bottom border-primary mx-auto text-center">
+        <h1>DASHBOARD</h1>
+      </div>
+    </div>
+    public {{ publicKeeps }} user {{ userKeeps }}
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {},
+  computed: {}
+};
+</script>
+
+<style></style>
